322312764926724ed353e42e4988b6507bbbb991
Luna-3003 <costafreitas.luna@gmail.com>
2026-06-09 10:38:53 -0300

    Botei no .env negada

--- config/conn.php
+++ config/conn.php
@@ -1,10 +1,19 @@
 <?php
+require_once __DIR__ . "env.php";
+
+carregar_env(__DIR__ . "/../../.env");
+
+$usuario = $_ENV["DB_USERNAME"];
+$senha = $_ENV["DB_PASSWORD"];
+$host = $_ENV["DB_HOST"];
+$porta = $_ENV["DB_PORT"];
+$banco = $_ENV["DB_DATABASE"];
 
 try {
-    $pdo = new PDO(
-        "mysql:host=banco;port=3306;dbname=lanotte;charset=utf8mb4",
-        "lanotte",
-        "lanotte123"
+        $pdo = new PDO(
+        "mysql:host={$host};port={$porta};dbname={$banco};charset=utf8mb4",
+        $usuario,
+        $senha
     );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
